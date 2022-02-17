642664879223aeacb78c3fb904ff2e47d39341f6
ltakuno <leonardo.takuno@gmail.com>
2022-02-17 11:14:25 -0300

    Add files via upload

---
+++ aula02 - exemplos.py
@@ -0,0 +1,81 @@
+# Listas
+# Sequencia de elementos. Delimitados por colchetes []
+
+# Listas heterogêneas: pode conter diferentes tipos de dados
+
+# listas vazias
+
+# Função print pode ser utilizada para exibir os itens contidos na lista
+
+# índice: especifica a posição de um item na lista
+# índice inicial é 0
+
+# índices negativos indicam posições referentes ao final da lista
+
+# listas são mutáveis: podem ser alteradas
+
+# append: adiciona item ao final da lista
+
+# repetição for para percorrer a lista
+
+# preencher lista com dados informados pelo usuário
+
+#-----------------------------------
+# principais funções
+
+# len: Retorna o tamanho de uma lista
+
+# count: Contar quantas vezes um intem aparece na lista
+
+# index: Retorna o índice da primeira ocorrência de um item
+# Se o item não for encontrado devolve uma exceção.
+
+# in: verificar se um determinado item existe em uma lista
+
+# for-in: utilizado para percorre a lista
+
+# insert: insere item em determinada posição
+
+# pop: remove o último item da lista
+
+# pop(indice): remove o item de um índice específico
+
+# remove(item): remove a primeira ocorrência de um item na lista
+#            - se um item não existe, um erro é gerado.
+
+# remover todas as ocorrencias de um item na lista
+
+# sort: ordenação da lista
+
+# reverse: inverte a lista
+
+# min: menor elemento
+
+# max: maior elemento
+
+# sum: somatorio da lista
+
+# concatenação da lista
+
+# cópia da lista
+
+#-----------------------------------
+# Tuplas
+
+# Sequencias em elementos que não podem ser alterados.
+# Delimitados por parenteses ()
+
+# tuplas vazias
+
+# tuplas de 1 elemento (tem uma vírgula no final)
+
+
+# acesso pelo índice
+
+# for pode ser utilizado para percorrer tuplas
+
+# concatenação de tuplas 
+
+# converter listas em tuplas
+
+# converter tuplas em listas
